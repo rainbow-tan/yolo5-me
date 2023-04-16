7ae64b0777f62599b2a34768d521d9e180411ae1
txh <txh@qq.com>
2023-04-16 11:53:10 +0800

    个人yolo5

---
+++ detect-me.py
@@ -0,0 +1,94 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+
+
+import os
+import sys
+from pathlib import Path
+
+import cv2
+import numpy as np
+import torch
+
+from utils.augmentations import letterbox
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[0]  # YOLOv5 root directory
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))  # add ROOT to PATH
+ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+
+from models.common import DetectMultiBackend
+from utils.general import (Profile, non_max_suppression, scale_boxes, xyxy2xywh)
+from utils.torch_utils import smart_inference_mode
+
+
+@smart_inference_mode()
+def run(
+        weights=r'D:\GitCode\yolo5-me\pts\2023-4-5-2.pt',  # model path or triton URL
+        source=r'D:\GitCode\yolo5-me\images_delect\1.png',  # file/dir/URL/glob/screen/0(webcam)
+        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
+        imgsz=(640, 640),  # inference size (height, width)
+        conf_thres=0.25,  # confidence threshold
+        iou_thres=0.45,  # NMS IOU threshold
+        max_det=1000,  # maximum detections per image
+):
+    source = str(source)
+
+    # Load model
+    device = torch.device('cuda:0')
+    model = DetectMultiBackend(weights, device=device, dnn=False, data=data, fp16=False)
+    stride, names, pt = model.stride, model.names, model.pt
+
+    # Dataloader
+    bs = 1  # batch_size
+
+    path = source
+    im0s = cv2.imread(path)  # BGR
+    print(type(im0s))
+    im = letterbox(im0s, imgsz, stride=32, auto=True)[0]  # padded resize
+    im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+    im = np.ascontiguousarray(im)  # contiguous
+
+    # Run inference
+    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
+    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    with dt[0]:
+
+        im = torch.from_numpy(im).to(model.device)
+        im = im.half() if model.fp16 else im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+
+    # Inference
+    with dt[1]:
+        pred = model(im, augment=False, visualize=False)
+
+    # NMS
+    with dt[2]:
+        pred = non_max_suppression(pred, conf_thres, iou_thres, None, False, max_det=max_det)
+
+    max_like = 0
+    max_people = None
+    for i, det in enumerate(pred):  # per image
+        seen += 1
+        p, im0 = path, im0s.copy()
+
+        gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+        if len(det):
+            det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], im0.shape).round()
+
+            for *xyxy, conf, cls in reversed(det):
+                xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                print(f"xywh:{xywh}")
+                print(f"conf:{conf}")
+                if conf > max_like:
+                    max_like = conf
+                    max_people = xywh
+    print(f"max_like:{max_like}")
+    print(f"max_people:{max_people}")
+    return max_people
+
+
+if __name__ == '__main__':
+    run()
---
+++ 记录.py
@@ -0,0 +1,18 @@
+"""
+python .\train.py --batch-size 36 --epochs 100 --data D:\Debug-Conda\yolov5\data\people.yaml --weight .\weights\yolov5s.pt --cache ram --device 0
+
+
+python .\detect.py --source D:\GitCode\yolo5-me\images_delect\96.png --weights D:\GitCode\yolo5-me\pts\2023-4-5-2.pt   --device 0 --save-txt
+
+"""
+import win32api
+import win32con
+
+#0 0.626562 0.283333 0.0132813 0.0555556
+width = win32api.GetSystemMetrics(win32con.SM_CXSCREEN)   #获得屏幕分辨率X轴
+
+height = win32api.GetSystemMetrics(win32con.SM_CYSCREEN)   #获得屏幕分辨率Y轴
+print(f'width:{width}')
+print(f'height:{height}')
+print(width*0.626562)
+print(height*0.283333)
